c90673fb716891cb4faeda7b2f07ff42c9059e56
Bora Demiral <69348563+Boragrammer@users.noreply.github.com>
2021-11-27 14:17:47 +0200

    Add files via upload

---
+++ task10.py
@@ -0,0 +1,10 @@
+speed = float(input('What is the speed of the vehicle in km/h? '))
+hour = int(input('How many hours has it traveled? '))
+distance = 0
+print('Hour\t', 'Distance Traveled')
+print('-' * 30)
+
+for i in range(1, hour + 1):
+    
+    print(format(i, '1.0f'), '\t\t', format(i * speed, '6.1f'))
+    distance += i * speed
